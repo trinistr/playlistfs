f19845ef57c560dc262dec25c6b6887d8f2b2d63
Alexander Bulancov <6594487+trinistr@users.noreply.github.com>
2018-09-16 03:37:52 +0300

    pfs_file_* function definitions

---
+++ src/files.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Playlist File System
+ * Copyright ©2018 Aleksandr Bulantcov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <playlistfs.h>
+
+pfs_file* pfs_file_create (char* path, char type) {
+	pfs_file* file = malloc (sizeof(*file));
+	if (!file)
+		return NULL;
+	file->path = path;
+	file->type = type;
+	file->nlinks = type == 'd' ? 2 : 1;
+	return file;
+}
+
+void pfs_file_free (pfs_file* file) {
+	free (file->path);
+	free (file);
+}
